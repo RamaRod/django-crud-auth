0d67d5783116d0bcad0f133998a7ff0a2106d17a
rama <ramirxdriguez@gmail.com>
2023-11-01 19:40:11 -0300

    actualizaciones

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -23,6 +23,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('task/', views.task, name='task'),
+    path('task/create/', views.create_task, name='create_task'),
+    path('task/<int:task_id>/', views.task_detail, name='task_detail'),
     path('logout/', views.signout, name='logout'),
     path('signin/', views.signin, name='signin'),
+    
 ]
--- task/views.py
+++ task/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+from .forms import TaskForm
+from .models import Task
 
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
-
 
 def signup(request):
 
@@ -36,16 +37,37 @@ def signup(request):
             "error": 'Las contrasenas no coinciden'
         })
 
-
 def task(request):
-    return render(request, 'task.html')
+    task = Task.objects.filter(user=request.user, datecompleted__isnull=True)
 
+    return render(request, 'task.html', {'task': task})
+
+def create_task(request):
+    if request.method == 'GET':
+        return render (request, 'create_task.html',{
+            'form': TaskForm
+        })
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit = False)
+            new_task.user = request.user
+            new_task.save()
+            return redirect ('task')
+        except ValueError:
+            return render(request, 'create_task.html', {
+                'form': TaskForm,
+                'error': 'Por favor ingrese un valor valido'
+            })
+
+def task_detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, 'task_detail.html', {'task': task})
 
 def signout(request):
 
     logout(request)
     return redirect('home')
-
 
 def signin(request):
     if request.method == 'GET':
